4e0bd5f19eaa47e9e8764a9923a7dd6d869464f1
Shelly Leung <pontificable@yahoo.com>
2018-10-16 09:53:01 -0700

    answer input field returns value, starting on feedback display

---
+++ src/components/answer-feedback.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Answers() {
+	//if answer matches question key-value pair, answer is correct
+	//else
+	return;
+}
--- src/components/qa-form.js
+++ src/components/qa-form.js
@@ -5,61 +5,70 @@
 	-CSS formatting	
 */
 
-
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Field, reduxForm, focus } from 'redux-form';
+import Input from './input';
 //import { addRack } from '../actions/protected-data';
 import './qa-form.css';
 
-export class Qa extends Component{
-	  //onSubmit(values) {
-    //return this.props.dispatch(addRack( values.latitude, values.longitude));
-  //}
+export class Qa extends Component {
+	//onSubmit(values) {
+	//return this.props.dispatch(addRack( values.latitude, values.longitude));
+	//}
 
-  //state = {
-   //latitude: null, longitude: null
-  //}
+	//state = {
+	//latitude: null, longitude: null
+	//}
 
+	//handleAnswer = (e) => {
+	//this.setState({
+	//answer: e.target.value
+	//})
+	//}
 
- //handleAnswer = (e) => {
-    //this.setState({
-      //answer: e.target.value
-    //})
-  //}
+	onSubmit(values) {
+		console.log(values);
 
+		// e.preventDefault();
+		//this.props.addDestination(this.state);
+		//this.setState({
+		//  answer: ''
+		//})
+	}
 
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    //this.props.addDestination(this.state);
-    //this.setState({
-    //  answer: ''
-    //})
-  }
-
-  render(){    
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div className = "qa-form">
-	    <div>[pH] Question Display</div>
-	    <input type = 'text' className = "input new-line-and-margin" autofocus cols = '38' rows = '1' /*onChange={this.handleAnswer} value={this.state.answer}*//>
-            <button type="submit" className = 'new-line-and-margin qa-button'>
-              Submit Answer
-            </button>
-          
-	  </div>
-        </form>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="qa-form">
+				<div>[pH] Question Display</div>
+				<form
+					onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+				>
+					<Field
+						label="My answer is: "
+						name="answer"
+						id="answer"
+						component={Input}
+						type="text"
+						className="input new-line-and-margin"
+						autoFocus
+						cols="38"
+						rows="1" /*onChange={this.handleAnswer} value={this.state.answer}*/
+					/>
+					<button
+						type="submit"
+						className="new-line-and-margin qa-button"
+						disabled={this.pristine || this.submitting}
+					>
+						Submit Answer
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default reduxForm({
-  form: 'answer',
-  onSubmitFail: (errors, dispatch) => dispatch(focus('answer'))
-
+	form: 'answer',
+	onSubmitFail: (errors, dispatch) => dispatch(focus('answer'))
 })(Qa);
-
-
