9b5b1c79a82665157241f1d62ceb1691f3b940b8
Ryan Ureta <rcureta95@gmail.com>
2018-10-18 13:35:16 -0700

    fix

--- src/components/qa-form.js
+++ src/components/qa-form.js
@@ -121,7 +121,8 @@ export class Qa extends Component {
 					</Formik>
 					{/* <Answers answer={this.state.answer} />  --This was a try at refactoring out the Answer Feedback but I couldn't get it to recognize certain props from here.*/}
 					<Next onClick={e => this.handleNext(e)} />
-					<button onClick={e => this.props.progButton(e)}>Show Progress</button>
+					<div className="answer-feedback">{this.state.feedback}{this.state.correctCount}</div>
+
 				</div>
 			);
 		}
@@ -174,7 +175,8 @@ export class Qa extends Component {
 				</Formik>
 				{/* <Answers answer={this.state.answer} />  --This was a try at refactoring out the Answer Feedback but I couldn't get it to recognize certain props from here.*/}
 				<Next onClick={e => this.handleNext(e)} />
-				<div className="answer-feedback">{this.state.feedback}{this.state.correctCount}</div>
+				<button onClick={e => this.props.progButton(e)}>Show Progress</button>
+
 			</div>
 		);
 	}
