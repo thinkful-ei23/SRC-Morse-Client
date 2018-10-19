e0e331d852c7f3b3e966aa128478008ca7375a7d
Ryan Ureta <rcureta95@gmail.com>
2018-10-19 13:05:50 -0700

    deleted redundant text

--- src/components/qa-form.js
+++ src/components/qa-form.js
@@ -56,7 +56,7 @@ export class Qa extends Component {
 			console.log('correct');
 			this.setState({
 				feedback:
-					"Yay! Keep at it! You'll be a spy in no time! Total Progress: ",
+					"Yay! Keep at it! You'll be a spy in no time! ",
 				correctCount: this.state.correctCount + 1
 			});
 			this.props.dispatch(correctAnswer(this.props.questions));
